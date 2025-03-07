8b62bdb61e08ea9cba0168a6d2c05673eb59062d
mourad ouafi <flyboxconcept@gmail.com>
2025-03-07 02:10:39 +0100

    Correction du problème de binding du port sur Heroku

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, jsonify, request
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route('/')
 def home():
-    return jsonify({"message": "API en ligne 🚀"}), 200
+    return "L'API fonctionne correctement !"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Utilisation du port de Heroku
+    app.run(host="0.0.0.0", port=port, debug=True)
